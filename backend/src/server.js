95cd6239c8a0bd12c86f1cf9ea78e1260b7bfc3e
Roba ABOUZAID <roba.abouzaid@ecole-hexagone.com>
2026-06-26 10:15:06 +0200

    feat(api): add health endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,20 @@ const studentRoutes = require("../routes/students.routes");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
     message: "API de Gestion des Étudiants",
     status: "OK"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "UP"
   });
 });
 
