2c8ead4a450b230d9fe3a359bf840e457b5ecb6d
Roba ABOUZAID <roba.abouzaid@ecole-hexagone.com>
2026-06-26 12:26:52 +0200

    fix: configure ESLint for Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,22 @@ import globals from "globals";
 
 export default [
   js.configs.recommended,
+
   {
     files: ["**/*.js"],
     languageOptions: {
       sourceType: "commonjs",
       globals: {
         ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
       },
     },
   },
